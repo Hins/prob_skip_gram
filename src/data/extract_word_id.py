24e3d2f533d41455b40aae255c6bfbd26253235f
panxiaotong <panxiaotong@xiaomi.com>
2018-09-04 19:13:04 +0800

    add part code

--- src/data/extract_word_id.py
+++ src/data/extract_word_id.py
@@ -2,3 +2,21 @@
 # @Time        : 2018/9/4 18:43
 # @Author      : panxiaotong
 # @Description : extract word id and context info
+
+import sys
+sys.path.append("..")
+from util.config import cfg
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("<extract_word_id> <input file> <word dict output file> <word id output file> <context output file>")
+        sys.exit()
+
+    word_dict = {}
+    word_list = []
+    context_list = []
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            elements = line.strip('\r\n').split(",")
+
+        f.close()
