d96da870ee7477af3152752dff80edef80daa62d
panxiaotong <panxiaotong@xiaomi.com>
2018-09-05 13:33:47 +0800

    adjust sample extration logic

--- src/data/extract_samples.py
+++ src/data/extract_samples.py
@@ -20,46 +20,49 @@ if __name__ == "__main__":
     nlp = StanfordCoreNLP(sys.argv[2])
     DBPedia_url = "https://api.dbpedia-spotlight.org/en/annotate?text="
     DBPedia_header = {'accept':'application/json'}
+    sub_context_window_size = cfg.context_window_size / 2
 
-    with open(sys.argv[9], 'w') as kb_entity_output:
-        with open(sys.argv[8], 'w') as dict_desc_output:
-            with open(sys.argv[7], 'w') as parser_output:
-                with open(sys.argv[6], 'w') as partofspeech_output:
-                    word_dict = {}
-                    with open(sys.argv[5], 'w') as context_output:
-                        with open(sys.argv[4], 'w') as word_output:
-                            with open(sys.argv[3], 'w') as dict_out:
-                                with open(sys.argv[1], 'r') as f:
-                                    for line in f:
-                                        line = line.strip('\r\n')
-                                        tokenize_list = nlp.word_tokenize(line)
+    word_dict_output = open(sys.argv[3], 'w')
+    word_output = open(sys.argv[4], 'w')
+    context_output = open(sys.argv[5], 'w')
+    partofspeech_output = open(sys.argv[6], 'w')
+    parser_output = open(sys.argv[7], 'w')
+    dict_desc_output = open(sys.argv[8], 'w')
+    kb_entity_output = open(sys.argv[9], 'w')
 
-                                        for word in tokenize_list:
-                                            word = str(word)
-                                            if word not in word_dict:
-                                                word_dict[word] = len(word_dict) + 1
+    word_dict = {}
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            line = line.strip('\r\n')
+            tokenize_list = nlp.word_tokenize(line)
 
-                                            print(wn.synsets(str(item))[0].definition())
-                                            r = requests.get(DBPedia_url + str(item), headers=DBPedia_header)
-                                            print(r.status_code)
-                                            print(str(r.text))
-                                            break
-                                        pos_list = nlp.pos_tag(line)
-                                        for item in pos_list:
-                                            print(str(item[0]))
-                                            print(str(item[1]))
-                                            break
-                                        dependency_parse_list = nlp.dependency_parse(line)
-                                        for item in dependency_parse_list:
-                                            print(str(item[0]))
-                                            print(str(item[1]))
-                                            print(str(item[2]))
-                                            break
-                                    f.close()
-                                dict_out.close()
-                            word_output.close()
-                        context_output.close()
-                    partofspeech_output.close()
-                parser_output.close()
-            dict_desc_output.close()
-        kb_entity_output.close()
+            for word in tokenize_list:
+                word = str(word)
+                if word not in word_dict:
+                    word_dict[word] = len(word_dict) + 1
+
+                print(wn.synsets(str(item))[0].definition())
+                r = requests.get(DBPedia_url + str(item), headers=DBPedia_header)
+                print(r.status_code)
+                print(str(r.text))
+                break
+            pos_list = nlp.pos_tag(line)
+            for item in pos_list:
+                print(str(item[0]))
+                print(str(item[1]))
+                break
+            dependency_parse_list = nlp.dependency_parse(line)
+            for item in dependency_parse_list:
+                print(str(item[0]))
+                print(str(item[1]))
+                print(str(item[2]))
+                break
+        f.close()
+
+    word_dict_output.close()
+    word_output.close()
+    context_output.close()
+    partofspeech_output.close()
+    parser_output.close()
+    dict_desc_output.close()
+    kb_entity_output.close()
